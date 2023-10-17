3ff52d5ca9d38255ede73e6c997c7cbe7b845325
Euseung <yousi7@naver.com>
2023-10-17 23:38:42 +0900

    feat: nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,21 +8,22 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.AUTH_GITHUB_ID ?? "",
-      clientSecret: process.env.AUTH_GITHUB_SECRET ?? "",
+      clientId: process.env.AUTH_GITHUB_ID as string,
+      clientSecret: process.env.AUTH_GITHUB_SECRET as string,
     }),
-    GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID ?? "",
-      clientSecret: process.env.AUTH_GOOGLE_SECRET ?? "",
-    }),
-    KakaoProvider({
-      clientId: process.env.AUTH_KAKAO_ID ?? "",
-      clientSecret: process.env.AUTH_KAKAO_SECRET ?? "",
-    }),
-    NaverProvider({
-      clientId: process.env.AUTH_NAVER_ID ?? "",
-      clientSecret: process.env.AUTH_NAVER_SECRET ?? "",
-    }),
+    // 아래와 같은 형식으로 소셜로그인 추가 가능하며, 현재 테스트는 깃허브만 사용
+    // GoogleProvider({
+    //   clientId: process.env.AUTH_GOOGLE_ID ?? "",
+    //   clientSecret: process.env.AUTH_GOOGLE_SECRET ?? "",
+    // }),
+    // KakaoProvider({
+    //   clientId: process.env.AUTH_KAKAO_ID ?? "",
+    //   clientSecret: process.env.AUTH_KAKAO_SECRET ?? "",
+    // }),
+    // NaverProvider({
+    //   clientId: process.env.AUTH_NAVER_ID ?? "",
+    //   clientSecret: process.env.AUTH_NAVER_SECRET ?? "",
+    // }),
     // ...add more providers here
   ],
 };
