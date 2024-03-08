05714478f88b9ea445b380438632054b7802a32f
Euseung <yousi7@naver.com>
2024-03-08 23:32:20 +0900

    api pdf

--- app/api/pdf/route.ts
+++ app/api/pdf/route.ts
@@ -1,40 +1,32 @@
-import { getUserId } from "@/data/data";
-import { db } from "@/lib/db";
-import { chats, pdf } from "@/lib/db/schema";
+import { dbCreateChat, dbCreatePdf, dbGetUserId } from "@/data/data";
 import { s3IntoPinecone } from "@/lib/pinecone";
 import { getS3Url } from "@/lib/s3";
 import { NextResponse } from "next/server";
-import { v4 as uuidv4 } from "uuid";
 
 export async function POST(req: Request) {
   try {
     const { file_key, file_name } = await req.json();
-    const userId = await getUserId();
-    let chatId: string;
+    const userId = await dbGetUserId();
+    if (!userId) return NextResponse.json([]);
 
-    const docs = await s3IntoPinecone(file_key);
+    // Vector DB - pinecone 저장
+    await s3IntoPinecone(file_key);
     // 추후 db작업
+    const chat_id = await dbCreateChat({
+      type: "pdf",
+      content: file_name,
+      userId,
+    });
 
-    const chat_id = await db
-      .insert(chats)
-      .values({
-        id: uuidv4(),
-        type: "pdf",
-        content: file_name,
-        userId,
-      })
-      .returning({ insertedId: chats.id });
-
-    chatId = chat_id[0].insertedId;
-
-    await db.insert(pdf).values({
+    // PDF 정보를 저장
+    const response = await dbCreatePdf({
       pdfName: file_name,
       pdfUrl: getS3Url(file_key),
-      chatId,
+      chatId: chat_id,
       fileKey: file_key,
     });
 
-    return NextResponse.json({ docs });
+    return NextResponse.json({ response }, { status: 200 });
   } catch (error) {
     console.log("[PDF_ERROR] : ", error);
     return new NextResponse("PDF Error", { status: 500 });
